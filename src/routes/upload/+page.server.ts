5f58bd409ed15da952657cc1bcf207f18be0ecfc
Dennis <dennis.groetzinger@web.de>
2024-06-05 09:10:58 +0200

    Install tailwind

--- src/routes/upload/+page.server.ts
+++ src/routes/upload/+page.server.ts
@@ -1,15 +1,28 @@
 import type { PageServerLoad } from './$types';
+import { supabase } from '$lib/supabaseClient';
+
 
 export const load = (async ({fetch}) => {
-    const text = `家住女贞路4号的德思礼夫妇总是得意地说他们是非常规矩的人家，拜托 ，拜托了。他们从来跟神秘古怪的事不沾边，因为他们根本不相信那些邪门歪道。
-弗农·德思礼先生在一家名叫格朗宁的公司做主管，公司生产钻机。他高大魁梧，胖得几乎连脖子都没有了，却蓄着一脸大胡子。德思礼太太是一个瘦削的金发女人。`;
+//     const text = `家住女貞路4號的德思禮夫婦總是得意地說他們是非常規矩的人家，拜託 ，拜託了。他們從來跟神祕古怪的事不沾邊，因爲他們根本不相信那些邪門歪道。
+// 弗農·德思禮先生在一家名叫格朗寧的公司做主管，公司生產鑽機。他高大魁梧，胖得幾乎連脖子都沒有了，卻蓄着一臉大鬍子。德思禮太太是一個瘦削的金髮女人。`;
     
-    fetch('/api/process', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: text })
-    })
-    return {};
+//     fetch('/api/process', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ text: text })
+//     })
+
+    const { data, error } = await supabase
+        .from('Texts')
+        .select('book_id');
+
+    if (error) {
+        console.error('Error fetching book IDs:', error);
+        return { texts: [] };
+    }
+
+
+    return { texts: data };
 }) satisfies PageServerLoad;
