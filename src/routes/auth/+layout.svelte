49c079548244b4e1c90dfd2cc3218f340a04f7ee
Dennis <dennis.groetzinger@web.de>
2024-08-05 10:25:59 +0200

    more auth boilerplate

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+	</nav>
+</header>
+<slot />
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<form method="POST" action="?/login">
+	<label>
+		Email
+		<input name="email" type="email" />
+	</label>
+	<label>
+		Password
+		<input name="password" type="password" />
+	</label>
+	<button>Login</button>
+	<button formaction="?/signup">Sign up</button>
+</form>
